6029b2f5b8a94a7da8781d17b97f0ff9a740fb4e
manav <manavkhadka2004@gmail.com>
2025-03-11 17:08:00 +0530

    [update] enhance homepage layout with new heading and icon; improve button styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
-
+import { ArrowRight } from "lucide-react";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex lg:flex-row flex-col gap-8 row-start-2 items-center sm:items-start">
-        
+      <main className="flex flex-col gap-8 row-start-2 items-center">
+        <div className="items-center text-center font-extrabold text-[40px] sm:text-[80px] w-full sm:w-full leading-tight tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+          Focus on Code, We Handle the Rest.
+        </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+        <div className="flex gap-4 items-center flex-row justify-center">
+          <a 
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 px-4"
             href="/deploy"
             target="_blank"
             rel="noopener noreferrer"
@@ -29,6 +31,11 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             Read our docs
+            
+            <div className="rounded-full bg-black/[.08] dark:bg-white/[.145] p-0.5 ml-3">
+              <ArrowRight/>
+            </div>
+
           </a>
         </div>
       </main>
